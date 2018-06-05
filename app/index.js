0c65b206704e66c80402fbe2bc3c7daaa1830718
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-06-05 17:17:16 +1200

    add and style checkmark to list

--- app/index.js
+++ app/index.js
@@ -8,7 +8,8 @@ EStyleSheet.build({
   $white: '#FFFFFF',
   $border: '#E2E2E2',
   $inputText: '#797979',
-  $lightGray: '#F0F0F0'
+  $lightGray: '#F0F0F0',
+  $darkText: '#343434'
 })
 
 export default () => <CurrencyList />

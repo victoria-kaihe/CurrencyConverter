3db58df7e165e734486be19e496fcede04202efa
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-06-19 09:35:46 +1200

    make currency checkmark dynamic

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -12,7 +12,9 @@ const TEMP_CURRENT_CURRENCY = 'CAD'
 class CurrencyList extends Component {
   static propTypes = {
     navigation: PropTypes.object,
-    dispatch: PropTypes.func
+    dispatch: PropTypes.func,
+    baseCurrency: PropTypes.string,
+    quoteCurrency: PropTypes.string
   }
   handlePress = (currency) => {
     const { type } = this.props.navigation.state.params
@@ -26,6 +28,10 @@ class CurrencyList extends Component {
   }
 
   render() {
+    let comparisonCurrency = this.props.baseCurrency
+    if (this.props.navigation.state.params.type === 'quote') {
+      comparisonCurrency = this.props.quoteCurrency
+    }
     return (
       <View style={{ flex: 1 }} >
         <StatusBar
@@ -37,7 +43,7 @@ class CurrencyList extends Component {
           renderItem={({ item }) => (
             <ListItem
               text={item}
-              selected={item === TEMP_CURRENT_CURRENCY}
+              selected={item === comparisonCurrency}
               onPress={() => this.handlePress(item)}
             />
           )}
@@ -49,4 +55,11 @@ class CurrencyList extends Component {
   }
 }
 
-export default connect()(CurrencyList)
+const mapStateToProps = (state) => {
+  return {
+    baseCurrency: state.currencies.baseCurrency,
+    quoteCurrency: state.currencies.quoteCurrency
+  }
+}
+
+export default connect(mapStateToProps)(CurrencyList)

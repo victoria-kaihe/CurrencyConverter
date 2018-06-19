3c255702cf32cbb0a863c2d8bbb936bfee0177c2
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-06-20 11:05:22 +1200

    create fetchLatest generator

--- app/config/sagas.js
+++ app/config/sagas.js
@@ -1,11 +1,18 @@
+import { takeEvery } from 'redux-saga/effects'
+
 // 1. swap currency
 // 2. change base currency happens
 // 3. upon initial app load
 
 import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION } from '../actions/currencies'
 
-const rootSaga = function* () {
+const fetchLatestConversionRates = function* (action) {
+  console.log('TODO:UPDATE THINGS', action)
   yield
+}
+
+const rootSaga = function* () {
+  yield takeEvery(GET_INITIAL_CONVERSION, fetchLatestConversionRates)
 }
 
 export default rootSaga

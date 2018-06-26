4f8914d561a85c7c788167ed4254ce7bac5bf360
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-06-27 11:01:12 +1200

    add try catch statement to catch errors

--- app/config/sagas.js
+++ app/config/sagas.js
@@ -1,16 +1,30 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, select, call } from 'redux-saga/effects'
 
-import { CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION, SWAP_CURRENCY } from '../actions/currencies';
+import {
+  CHANGE_BASE_CURRENCY,
+  GET_INITIAL_CONVERSION,
+  SWAP_CURRENCY
+} from '../actions/currencies'
+
+const getLatestRate = currency => fetch(`https://fixer.handlebarlabs.com/latest?base=${currency}`)
 
 const fetchLatestConversionRates = function* (action) {
-  console.log('TODO: Update the things.', action);
-  yield;
-};
+  try {
+    let currency = action.currency
+    if (currency === undefined) {
+      currency = yield select(state => state.currencies.baseCurrency)
+    }
+    const response = yield call(getLatestRate, currency)
+    const result = yield response.json()
+  } catch (e) {
+    console.log('Saga error', e)
+  }
+}
 
 const rootSaga = function* () {
-  yield takeEvery(GET_INITIAL_CONVERSION, fetchLatestConversionRates);
-  yield takeEvery(CHANGE_BASE_CURRENCY, fetchLatestConversionRates);
-  yield takeEvery(SWAP_CURRENCY, fetchLatestConversionRates);
-};
+  yield takeEvery(GET_INITIAL_CONVERSION, fetchLatestConversionRates)
+  yield takeEvery(CHANGE_BASE_CURRENCY, fetchLatestConversionRates)
+  yield takeEvery(SWAP_CURRENCY, fetchLatestConversionRates)
+}
 
 export default rootSaga;

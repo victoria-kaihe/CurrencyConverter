624679bea1e09738401a4afbf5a2e3ef423246dc
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-06-27 10:25:00 +1200

    configure sagas

--- app/config/sagas.js
+++ app/config/sagas.js
@@ -1,20 +1,16 @@
-import { takeEvery } from 'redux-saga/effects'
+import { takeEvery } from 'redux-saga/effects';
 
-// 1. swap currency
-// 2. change base currency happens
-// 3. upon initial app load
-
-import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION } from '../actions/currencies'
+import { CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION, SWAP_CURRENCY } from '../actions/currencies';
 
 const fetchLatestConversionRates = function* (action) {
-  console.log('TODO:UPDATE THINGS', action)
-  yield
-}
+  console.log('TODO: Update the things.', action);
+  yield;
+};
 
 const rootSaga = function* () {
-  yield takeEvery(GET_INITIAL_CONVERSION, fetchLatestConversionRates),
-    yield takeEvery(SWAP_CURRENCY, fetchLatestConversionRates),
-    yield takeEvery(CHANGE_BASE_CURRENCY, fetchLatestConversionRates)
-}
+  yield takeEvery(GET_INITIAL_CONVERSION, fetchLatestConversionRates);
+  yield takeEvery(CHANGE_BASE_CURRENCY, fetchLatestConversionRates);
+  yield takeEvery(SWAP_CURRENCY, fetchLatestConversionRates);
+};
 
-export default rootSaga
+export default rootSaga;
